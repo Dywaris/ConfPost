8b9a4b41d5bc53c009a69dfbc4c485ccb3f67938
julien dezerable <Julien81298>
2021-02-08 11:39:39 +0100

    premier commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$db="conf_post";
+$dbhost="localhost";
+$dbport=3307;
+$dbuser="root";
+$dbpasswd="";
+
+$pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
+$pdo->exec("SET CHARACTER SET utf8");
+
+$stmt = $pdo->prepare("SELECT * FROM hellotable WHERE libelle='Hello world'");
+$stmt->execute();
+
+$res = $stmt->fetch();
+echo $res['libelle'];
+$pdo = null;
+
+?>
