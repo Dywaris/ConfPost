1249fa6d8639d31e852647efe8339a2c3cfad647
julien dezerable <Julien81298>
2021-02-08 15:22:48 +0100

    commit

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <?php
 
-$db="conf_post";
-$dbhost="localhost";
-$dbport=3307;
-$dbuser="root";
-$dbpasswd="";
+ $db="conf_post";
+ $dbhost="localhost";
+ $dbport=3306;
+ $dbuser="julien";
+ $dbpasswd="root";
 
-$pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
-$pdo->exec("SET CHARACTER SET utf8");
+ $pdo = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
+ $pdo->exec("SET CHARACTER SET utf8");
 
-$stmt = $pdo->prepare("SELECT * FROM hellotable WHERE libelle='Hello world'");
-$stmt->execute();
+ $stmt = $pdo->prepare("SELECT * FROM hellotable WHERE libelle='Hello world'");
+ $stmt->execute();
 
-$res = $stmt->fetch();
+ $res = $stmt->fetch();
 echo $res['libelle'];
 $pdo = null;
-
 
 
 ?>
